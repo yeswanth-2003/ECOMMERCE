4fca9e4d3889693ba388d352bd6cb38be07bb429
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2025-01-06 17:56:43 +0530

    Update resetpassword.js

--- controllers/resetpassword.js
+++ controllers/resetpassword.js
@@ -1,26 +1,23 @@
 const User = require('../models/registerschema');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 const resetpassword = async (req,res) =>{
     const {userId,newPassword} = req.body;
-    const hashedPassword = await bcrypt.hash(newPassword,10);
+    // const hashedPassword = await bcrypt.hash(newPassword,10);
     const emailExist = await User.findOne({userId});
     if(emailExist){
         try {
             const user = await User.findOne({userId});
-            await user.updateOne({$set:{Password:hashedPassword}})
+            await user.updateOne({$set:{Password}})
             user.save();
             res.status(201).json({message:"Password reset successfully",user});
     }
     catch (error) {
         res.status(500).json({message:"Something went wrong",error});
     }
-
     }
-
     else{
-        return res.status(400).json({message:"Invalid token"});
+        console.log('Enter valid Email')
     }
 
 }
