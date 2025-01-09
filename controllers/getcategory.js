93fac554589bf7653d286fc36edf986c37809d7f
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2025-01-09 12:54:34 +0530

    fffffffffffg

--- controllers/getcategory.js
+++ controllers/getcategory.js
@@ -1,7 +1,18 @@
-const Category = require('../models/categoryschema')
+const Categorys = require('../models/categoryschema');
 
-const Getcategory=async(req,res)=>{
-    
+const getallcategorys = async (req, res) => {
+    try {
+        const categorys = await Categorys.find();
+        const result = []; 
+        categorys.forEach((category) => {
+            if (category.parent === null) {
+                result.push(category.name); 
+            }
+        });
+        res.status(200).json(result); 
+    } catch (error) {
+        res.status(500).json({ message: "something went wrong", error });
+    }
 }
 
-module.exports=Getcategory
+module.exports = getallcategorys;
--- routes/categoryroutes.js
+++ routes/categoryroutes.js
@@ -1,8 +1,10 @@
 const Postcategory = require("../controllers/postcategory") ;
+const GetCategory = require('../controllers/getcategory')
 const express = require ('express');
 
 const router = express.Router()
 
 router.post('/post',Postcategory);
+router.get('/getcategory',GetCategory)
 
 module.exports = router;
