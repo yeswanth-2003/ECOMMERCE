617d4f1534cda969db19ce9f265c56bfd55f2cc2
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2025-01-17 13:14:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const CartRoutes = require('./routes/cartroutes');
 const CategoryRoutes = require('./routes/categoryroutes');
 const CarouselRoutes = require('./routes/carouselroutes');
 const OrderRoutes = require('./routes/orderroutes');
+const Wishroutes = require('./routes/wishlistroutes')
 
 
 
@@ -20,7 +21,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -30,6 +33,7 @@ app.use("/cart",CartRoutes);
 app.use('/category',CategoryRoutes);
 app.use('/carousel',CarouselRoutes);
 app.use('/order',OrderRoutes);
+app.use('/wishlist',Wishroutes);
 
 
 mongoose  
